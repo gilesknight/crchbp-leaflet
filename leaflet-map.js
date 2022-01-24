fc06be4e416f5a7491832ecbcf717a09da278fd1
Giles Knight <62376886+gilesnknight@users.noreply.github.com>
2022-01-24 10:11:48 +0800

    bushfire slider

--- leaflet-map.js
+++ leaflet-map.js
@@ -10,9 +10,23 @@ var osmLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
 var osmURL = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var osmAttrib = '&copy; ' + osmLink;
 
+//ibra tiles
+var burnLink = '<a href="https://catalogue.data.wa.gov.au/dataset/dbca-fire-history">DBCA</a>';
+var burn2021URL = '/data/burnScar2021/{z}/{x}/{y}.png';
+var burn2020URL = '/data/burnScar2020/{z}/{x}/{y}.png';
+var burn2019URL = '/data/burnScar2019/{z}/{x}/{y}.png';
+var burn2018URL = '/data/burnScar2018/{z}/{x}/{y}.png';
+var burn2017URL = '/data/burnScar2019/{z}/{x}/{y}.png';
+var burnAttrib = '&copy; ' + burnLink;
+
 // Create map tiles
 var esriMap = L.tileLayer(esriURL, {attribution: esriAttrib});
 var osmMap = L.tileLayer(osmURL, {attribution: osmAttrib});
+var burn2021Map = L.tileLayer(burn2021URL, {attribution: burnAttrib, time: "2021"});
+var burn2020Map = L.tileLayer(burn2020URL, {attribution: burnAttrib, time: "2020"});
+var burn2019Map = L.tileLayer(burn2019URL, {attribution: burnAttrib, time: "2019"});
+var burn2018Map = L.tileLayer(burn2018URL, {attribution: burnAttrib, time: "2018"});
+// var burn2017Map = L.tileLayer(burn2017URL, {attribution: burnAttrib, time: "2017"});
 
 // create map and set view
 var map = L.map('map',{layers: [esriMap]}).setView({lon: 133.7751, lat: -25.2744}, 5);
@@ -21,7 +35,24 @@ var baseLayers = {
     "ESRI Satellite": esriMap,
     "OSM Mapnik": osmMap
 };
+
+// var burnScars = {
+//     "2021": burn2021Map,
+//     "2020": burn2020Map,
+//     "2019": burn2019Map
+// };
+
 L.control.layers(baseLayers).addTo(map);
+
+
+var burnScars = [burn2021Map, burn2020Map, burn2019Map, burn2018Map];
+layerGroup = L.layerGroup(burnScars);
+var sliderControl = L.control.sliderControl({
+    layer: layerGroup,
+    follow: true
+  });
+map.addControl(sliderControl);
+sliderControl.startSlider();
 
 
 // show the scale bar on the lower left corner
@@ -54,6 +85,13 @@ var burnScarStyle = {
     "weight": 0,
     "opacity": 1,
     fillOpacity: 0.5
+};
+
+var gridStyle = {
+    "color": "orange",
+    "weight": 0,
+    "opacity": 1,
+    fillOpacity: 0
 };
 
 
@@ -112,33 +150,83 @@ map.on('baselayerchange', function(e, layer) {
 
 // burns scar layer
 
-function onEachBurnFeature(feature, layer) {
-    layer.on({
-        mouseover: function(e) {
-            layer.setStyle({weight: 1});
-        },
-        mouseout: function(e) {
-            layer.setStyle({weight: 0});
-        }
-    });
-}
+// function onEachBurnFeature(feature, layer) {
+//     layer.on({
+//         mouseover: function(e) {
+//             layer.setStyle({weight: 1});
+//         },
+//         mouseout: function(e) {
+//             layer.setStyle({weight: 0});
+//         }
+//     });
+// }
 
-var burnScarLayer = L.geoJSON(
-    burnScar, {
-        style: burnScarStyle,
-        onEachFeature: onEachBurnFeature
-    }
-).bindPopup( function (layer) {
-    var year =  layer.feature.properties.fih_year1;
-    var type =  layer.feature.properties.type;
-    return "<b>Type:</b> " + type + "<br><b>Year:</b> " + year;
-});
+// var burnScarLayer = L.geoJSON(
+//     burnScar, {
+//         style: burnScarStyle,
+//         onEachFeature: onEachBurnFeature
+//     }
+// ).bindPopup( function (layer) {
+//     var year =  layer.feature.properties.fih_year1;
+//     var type =  layer.feature.properties.type;
+//     return "<b>Type:</b> " + type + "<br><b>Year:</b> " + year;
+// });
+
+
+// function onEachGridFeature(feature, layer) {
+//     layer.on({
+//         mouseover: function(e) {
+//             layer.setStyle({weight: 1});
+//         },
+//         mouseout: function(e) {
+//             layer.setStyle({weight: 0});
+//         }
+//     });
+// }
+
+
+// var grid = L.geoJSON(grid, {
+//     style: gridStyle,
+//     onEachFeature: onEachGridFeature
+// });
+
+// grid.addTo(map);
+
 
 // data layers for control
-var dataLayers = {
-    "Burn scar data": burnScarLayer
-};
+// var dataLayers = {
+//     "Burn scar data": burnScarLayer
+//     // "Burn grid": grid
+// };
 
-// add control to map
-L.control.layers(null,dataLayers).addTo(map);
+// // add control to map
+// L.control.layers(dataLayers).addTo(map);
 
+
+
+
+
+// var oneoneoneone = L.tileLayer.wms("http://localhost:8080/geoserver/wms", {
+//   layers: 'map:1111',
+//   transparent: true,
+//   version: '1.1.0',
+//   format: 'image/png',
+//   time: '1111'
+// });
+
+// var oneoneonetwo = L.tileLayer.wms("http://localhost:8080/geoserver/wms", {
+//   layers: 'map:1112',
+//   transparent: true,
+//   version: '1.1.0',
+//   format: 'image/png',
+//   time: '1112'
+// });
+
+// var years = [oneoneoneone, oneoneonetwo];
+// layerGroup = L.layerGroup(years);
+// var sliderControl = L.control.sliderControl({
+//   layer: layerGroup,
+//   follow: true
+// });
+// map.addControl(sliderControl);
+// sliderControl.startSlider();
